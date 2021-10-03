d109864f635cfe3f85bc62e6dce46e7ec2465289
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-03 12:40:33 +0200

    listening to events

--- section-2-1/app.js
+++ section-2-1/app.js
@@ -4,13 +4,22 @@ const app = Vue
       firstName: 'Gregor',
       lastName: 'Sondermeier',
       url: 'https://google.com',
-      rawUrl: '<a href="https://google.com" target="_blank">Link</a>'
+      rawUrl: '<a href="https://google.com" target="_blank">Link</a>',
+      age: 32,
     }),
     methods: {
-      // error functions won't work because of differences in 'this' binding
       getFullName() {
-        return `${this.firstName} ${this.lastName.toUpperCase()}`
-      }
+        return `${this.firstName} ${this.lastName}`
+      },
+      setLastName(event) {
+        this.lastName = event.target.value;
+      },
+      incrementAge() {
+        this.age++;
+      },
+      decrementAge() {
+        this.age--;
+      },
     }
   });
 
