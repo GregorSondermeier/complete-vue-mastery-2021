b54ad2c9056459ce5105817dedc1aa5d685dbb79
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-03 11:54:42 +0200

    back to only one instance

--- section-2-1/app.js
+++ section-2-1/app.js
@@ -1,14 +1,9 @@
-const createMyApp = ({firstName, lastName}) =>
-  Vue
-    .createApp({
-      data: () => ({
-        firstName,
-        lastName,
-      }),
-    });
+const app = Vue
+  .createApp({
+    data: () => ({
+      firstName: 'Gregor',
+      lastName: 'Sondermeier',
+    }),
+  });
 
-const app1 = createMyApp({firstName: 'Gregor', lastName: 'Sondermeier'});
-const app2 = createMyApp({firstName: 'John', lastName: 'Doe'});
-
-app1.mount('#app-1');
-app2.mount('#app-2');
+app.mount('#app');
