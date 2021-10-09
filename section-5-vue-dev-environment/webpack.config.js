b50eec2c521e714c3bf57c5154a79f48964c22f3
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-09 15:47:23 +0200

    configure babel

--- section-5-vue-dev-environment/webpack.config.js
+++ section-5-vue-dev-environment/webpack.config.js
@@ -4,4 +4,13 @@ module.exports = {
     path: `${__dirname}/dist`,
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js%/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      }
+    ]
+  },
 };
