bac04538b758546150d85312077af3e7120dbd7a
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-09 13:34:11 +0200

    playing around with lifecycle hooks

--- section-4-beyond-the-fundamentals/app.js
+++ section-4-beyond-the-fundamentals/app.js
@@ -5,12 +5,41 @@ const app = Vue.createApp({
     };
   },
 
+  beforeCreate() {
+    console.log('beforeCreate():', this.message, this.$el);
+  },
+  created() {
+    console.log('created()', this.message, this.$el);
+  },
+  beforeMount() {
+    console.log('beforeMount()', this.message, this.$el);
+  },
+  mounted() {
+    console.log('mounted()', this.message, this.$el);
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()', this.message, this.$el);
+  },
+  updated() {
+    console.log('updated()', this.message, this.$el);
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()', this.message, this.$el);
+  },
+  unmounted() {
+    console.log('unmounted()', this.message, this.$el);
+  },
+
   template: `
-    {{message}}
+    <p>{{message}}</p>
   `,
 });
 
 let vm;
 setTimeout(() => {
   vm = app.mount('#app');
-}, 3000);
+}, 1000);
+
+setTimeout(() => {
+  app.unmount();
+}, 6000);
