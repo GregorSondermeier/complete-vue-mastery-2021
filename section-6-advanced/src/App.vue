e7acfc6b5711daeac3beb1d5314edc395e6c0023
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-10 17:07:11 +0200

    props

--- section-6-advanced/src/App.vue
+++ section-6-advanced/src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <p>Hey!</p>
-  <greeting :message="message" />
+  <h3>Hey!</h3>
+  <Greeting :message="message" />
+  <User :age="age"/>
 </template>
 
 <script>
-import Greeting from "./components/Greeting";
+import Greeting from "./components/Greeting/Greeting.vue";
+import User from "./components/User/User";
 
 export default {
   name: 'App',
 
   components: {
     Greeting,
+    User,
   },
 
   data() {
     return {
       message: "Hello World!",
+      age: 20,
     }
   }
 }
---
+++ section-6-advanced/src/components/User/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>The user is {{ age }} years old.</p>
+</template>
+
+<script>
+export default {
+  name: "User",
+
+  props: {
+    age: Number,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+$green: #019340;
+
+p {
+  color: $green;
+}
+</style>
