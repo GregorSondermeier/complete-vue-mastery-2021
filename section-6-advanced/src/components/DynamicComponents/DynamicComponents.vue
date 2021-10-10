b7050a951ac07d8cddc7d72ca8f49e33bd0066f5
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-10 18:14:29 +0200

    dynamic components

--- section-6-advanced/src/components/DynamicComponents/DynamicComponents.vue
+++ section-6-advanced/src/components/DynamicComponents/DynamicComponents.vue
@@ -1,10 +1,33 @@
 <template>
   <h2>Dynamic components</h2>
+
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
 </template>
 
 <script>
+import About from './About/About.vue';
+import Home from './Home/Home.vue';
+
 export default {
   name: "DynamicComponents",
+
+  components: {
+    About,
+    Home,
+  },
+
+  data() {
+    return {
+      componentName: 'Home',
+    };
+  },
 };
 </script>
 
