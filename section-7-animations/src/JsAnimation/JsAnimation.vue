01178b7b66568e26df479f386f59b460f7528aac
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-16 13:20:30 +0200

    js zoom animation

--- section-7-animations/src/JsAnimation/JsAnimation.vue
+++ section-7-animations/src/JsAnimation/JsAnimation.vue
@@ -4,6 +4,7 @@
   <button type="button" @click="toggleFlag()">Toggle Flag</button>
 
   <transition
+    :css="false"
     @before-enter="onBeforeEnter"
     @enter="onEnter"
     @after-enter="onAfterEnter"
@@ -35,21 +36,46 @@ export default {
     },
     onEnter(element, done) {
       console.log('onEnter(element, done)', element);
-      done();
+      const animation = element.animate([
+        { transform: 'scale3d(0,0,0)' },
+        {},
+      ], {
+        duration: 1000,
+      });
+
+      animation.onfinish = () => {
+        done();
+      };
     },
     onAfterEnter(element) {
       console.log('onAfterEnter(element)', element);
     },
     onBeforeLeave(element) {
-        console.log('onBeforeLeave(element)', element);
+      console.log('onBeforeLeave(element)', element);
     },
     onLeave(element, done) {
-        console.log('onLeave(element, done)', element);
-      done();
+      const animation = element.animate([
+        {},
+        { transform: 'scale3d(0,0,0)' },
+      ], {
+        duration: 1000,
+      });
+
+      animation.onfinish = () => {
+        done();
+      };
     },
     onAfterLeave(element) {
-        console.log('onAfterLeave(element)', element);
-    }
+      console.log('onAfterLeave(element)', element);
+    },
   },
 }
 </script>
+
+<style scoped>
+h3 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
