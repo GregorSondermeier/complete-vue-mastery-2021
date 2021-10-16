0621ee8c0f0aefd89baa27d1bced966e6b486d52
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-10-16 11:48:57 +0200

    use transition component to create simple css transitions

--- section-7-animations/src/App.vue
+++ section-7-animations/src/App.vue
@@ -1,10 +1,36 @@
-<template></template>
+<template>
+  <button type="button" @click="toggleFlag()">Toggle</button>
+
+  <transition name="fade">
+    <h2 v-if="flag">Hello World!</h2>
+  </transition>
+</template>
 
 <script>
 export default {
   name: "App",
+
+  data() {
+    return {
+      flag: false,
+    }
+  },
+
+  methods: {
+    toggleFlag() {
+      this.flag = !this.flag;
+    }
+  }
 };
 </script>
 
 <style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s linear;
+}
 </style>
