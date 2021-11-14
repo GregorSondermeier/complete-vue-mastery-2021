ca4049c41537c79aba586c10b768cab943f8717b
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2021-11-14 15:30:47 +0100

    custom error messages

--- section-9-music/src/plugins/vee-validate.js
+++ section-9-music/src/plugins/vee-validate.js
@@ -10,10 +10,11 @@ import {
   required,
 } from '@vee-validate/rules';
 import {
+  configure,
+  defineRule,
   ErrorMessage as VeeErrorMessage,
   Form as VeeForm,
   Field as VeeField,
-  defineRule,
 } from 'vee-validate';
 
 /**
@@ -26,13 +27,39 @@ export const VeeValidate = {
     app.component('VeeErrorMessage', VeeErrorMessage);
 
     defineRule('alphaSpaces', alphaSpaces);
-    defineRule('confirmed', confirmed);
+    defineRule('passwordsMustMatch', confirmed);
     defineRule('email', email);
     defineRule('max', max);
     defineRule('maxValue', maxValue);
     defineRule('min', min);
     defineRule('minValue', minValue);
     defineRule('notOneOf', notOneOf);
+    defineRule('notOneOfTheseCountries', notOneOf);
     defineRule('required', required);
+    defineRule('acceptTermsOfService', required);
+
+    configure({
+      generateMessage: (context) => {
+        const messages = {
+          acceptTermsOfService: 'You must accept the Terms of Service.',
+          alphaSpaces: `The field ${context.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${context.field} must be a valid email.`,
+          max: `The field ${context.field} is too long.`,
+          maxValue: `The field ${context.field} is too high.`,
+          min: `The field ${context.field} is too short.`,
+          minValue: `The field ${context.field} is too low.`,
+          notOneOf: `You are not allowed to use this value for the field ${context.field}.`,
+          notOneOfTheseCountries:
+            'Due to restrictions, we do not accept users from this location',
+          passwordsMustMatch: 'The passwords do not match.',
+          required: `The field ${context.field} is required.`,
+        };
+
+        return (
+          messages[context.rule.name] ||
+          `The field ${context.field} is invalid.`
+        );
+      },
+    });
   },
 };
